58ec25e534a0f98b989e3730d0d4b883556a9c8b
Dulguun Narantuya <19b1num0011@stud.num.edu.mn>
2025-03-18 15:30:40 +0800

    Created Repo

---
+++ src/test/java/num/edu/freelanceHub/FreelanceHubApplicationTests.java
@@ -0,0 +1,13 @@
+package num.edu.freelanceHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelanceHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
